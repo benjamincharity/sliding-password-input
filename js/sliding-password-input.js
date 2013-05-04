6f4d411f75a0a34af0efe0dd7ad7aed7a86916dd
Benjamin Charity <ben@benjamincharity.com>
2013-05-04 15:56:42 -0400

    moved to new repo, set up files

---
+++ js/sliding-password-input.js
@@ -0,0 +1,40 @@
+var s,
+SlidingPasswordInput = {
+
+  settings : {
+    firstInput: $('.main'),
+    secondInput: $('.main'),
+    nestedExample : {
+      first: true,
+      second: true,
+      third: true,
+      fourth: true,
+      fifth : ['one', 'two', 'three', 'four', 'five', 'six']
+    },
+
+    foo: 'bar'
+
+  },
+
+  init: function () {
+    s = this.settings;
+
+    this.nextMethod();
+    this.anotherMethod();
+  },
+
+  validateFormat: function () {
+  },
+
+  testStrength: function () {
+  },
+
+  comparePasswords: function () {
+  }
+
+};
+
+
+$(function () {
+  SlidingPasswordInput.init();
+});
